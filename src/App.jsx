2d5ecb70fc442f5b14b4b97b447ba7aa5efd94dd
Ishimwe Muhire Sadi <yanicksadi10@gmail.com>
2026-04-26 03:17:58 +0200

    Connect Components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { usestate } from "react";
+import data from "./data/data.json";
+import FileExplorer from "./components/FileExplorer.jsx";
+import PropertiesPanel from "./components/PropertiesPanel.jsx";
+import SearchBar from "./components/SearchBar.jsx"
+
 function App(){
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [search, setSearch] = useState("");
+
   return (
-    <div>
-      <h1>SecureVault Dashboard</h1>
-    </div>
+      <div style={{ background: "#0B0F19", color: "#fff", height: "100vh" }}>
+      
+      {/* search  */}
+      <div style={{ padding: "10px" }}>
+        <searchBar onSearch={setSearch} />
+      </div>
+      
+      {/* the main layout */}
+      <div style={{ display: "flex", height: "90%" }}>
+        {/* the side Bar */}
+        <div style={{width: "40%", borderRight: "1px solid #374151" }}>
+          <fileExplorer data={data} onSelect={setSelectedFile} />
+        </div>
+        {/* {The details Side} */}
+        <div style={{ width: "60%" }}>
+          <propertiesPanel file={selectedFile}/>
+        </div>
+      </div>
+     </div>
+    
   );
 }
 
